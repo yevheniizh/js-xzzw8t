8138ff6d2de224d530944250f5d9e2da0ef16392
yevheniizh <69813005+yevheniizh@users.noreply.github.com>
2023-09-20 22:07:05 +0300

    Add coloured waves

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ import * as THREE from 'three';
 const vertexShader = `
   varying vec2 vUv;
   uniform float time;
+  varying vec3 vPosition;
 
   void main() {
-    vUv = uv;
-    vec3 new_position = position;
-    new_position.x += sin( time + position.y ) * 5.0;
-    new_position.y += cos( time + position.x ) * 5.0;
-    new_position.z += sin( time + position.x ) * 5.0;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(new_position, 1);
-  }`;
+    vPosition = position;
+    float sin1 = sin((position.x + position.y) * 0.2 + time * 0.5);
+    float sin2 = sin((position.x - position.y) * 0.4 + time * 2.0);
+    float sin3 = sin((position.x + position.y) * -0.6 + time);
+    vec3 updatePosition = vec3(position.x, position.y, position.z + sin1 * 50.0 + sin2 * 10.0 + sin3 * 8.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(updatePosition, 1.0);
+  }
+`;
 const fragmentShader = `
-  varying vec2 vUv;
+  varying vec3 vPosition;
+  uniform float time;
+
+  const float duration = 8.0;
+  const float delay = 2.0;
+
+  vec3 convertHsvToRgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+  }
+
   void main() {
-    gl_FragColor = vec4(0.,vUv, 1.0);
+    float now = clamp((time - delay) / duration, 0.0, 1.0);
+    float opacity = (1.0 - length(vPosition.xy / vec2(512.0))) * now;
+    vec3 v = normalize(vPosition);
+    vec3 rgb = convertHsvToRgb(vec3(0.5 + (v.x + v.y + v.x) / 40.0 + time * 0.1, 0.4, 1.0));
+    gl_FragColor = vec4(rgb, opacity);
   }`;
 
 class Sketch {
@@ -82,12 +99,12 @@ class Sketch {
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
 
-    this.mesh.position.set(0, -100, 0);
+    this.mesh.position.set(0, -175, 0);
     this.mesh.rotation.set(-Math.PI / 3, 0, 0);
   }
 
   render() {
-    this.time += 0.025;
+    this.time += 0.02;
 
     this.material.uniforms.time.value = this.time;
 
