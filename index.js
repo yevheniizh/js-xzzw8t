3f7f18d1dd9eb7b37e264903fe35a70aa36d3540
yevheniizh <69813005+yevheniizh@users.noreply.github.com>
2023-09-20 23:31:41 +0300

    Add camera move on mouse move

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const vertexShader = `
 
   void main() {
     vPosition = position;
+
     float sin1 = sin((position.x + position.y) * 0.2 + time * 0.5);
     float sin2 = sin((position.x - position.y) * 0.4 + time * 2.0);
     float sin3 = sin((position.x + position.y) * -0.6 + time);
     vec3 updatePosition = vec3(position.x, position.y, position.z + sin1 * 50.0 + sin2 * 10.0 + sin3 * 8.0);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(updatePosition, 1.0);
-  }
-`;
+  }`;
+
 const fragmentShader = `
   varying vec3 vPosition;
   uniform float time;
@@ -36,14 +37,25 @@ const fragmentShader = `
   }`;
 
 class Sketch {
+  onMouseMove = (event) => {
+    this.mouseX = event.clientX - this.windowHalfX;
+    this.mouseY = event.clientY - this.windowHalfY;
+  };
+
   constructor(options) {
-    this.container = options.domElement;
-    this.scene = new THREE.Scene();
+    this.container = options.canvas;
+    this.input = options.input;
+    this.button = options.button;
+
     this.width = window.innerWidth;
     this.height = window.innerHeight;
 
-    this.raycaster = new THREE.Raycaster();
-    this.pointer = new THREE.Vector2();
+    this.scene = new THREE.Scene();
+    this.target = new THREE.Vector3();
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.windowHalfX = this.width / 2;
+    this.windowHalfY = this.height / 2;
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.container,
@@ -64,9 +76,63 @@ class Sketch {
 
     this.time = 0;
 
+    this.setupMouseEvents();
+    this.setupAudio();
     this.addObjects();
     this.setupResize();
     this.render();
+  }
+
+  setupMouseEvents() {
+    window.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  setupAudio() {
+    this.listener = new THREE.AudioListener();
+    this.audio = new THREE.Audio(this.listener);
+
+    this.input.addEventListener(
+      'change',
+      (event) => {
+        const files = event.target.files;
+        const reader = new FileReader();
+
+        reader.onload = (file) => {
+          const arrayBuffer = file.target.result;
+
+          this.listener.context.decodeAudioData(arrayBuffer, (audioBuffer) => {
+            this.audio.setBuffer(audioBuffer);
+          });
+        };
+
+        reader.readAsArrayBuffer(files[0]);
+      },
+      false
+    );
+
+    this.button.addEventListener('click', () => this.audio.play(), false);
+
+    // // create an AudioListener and add it to the camera
+    // this.listener = new THREE.AudioListener();
+    // this.camera.add(this.listener);
+
+    // // create a global audio source
+    // this.sound = new THREE.Audio(this.listener);
+
+    // // load a sound and set it as the Audio object's buffer
+    // this.audioLoader = new THREE.AudioLoader();
+    // this.audioLoader.load('sounds/ambient.ogg', function (buffer) {
+    //   this.sound.setBuffer(buffer);
+    //   this.sound.setLoop(true);
+    //   this.sound.setVolume(0.5);
+    //   this.sound.play();
+    // });
+
+    // // create an AudioAnalyser, passing in the sound and desired fftSize
+    // this.analyser = new THREE.AudioAnalyser(this.sound, 32);
+
+    // // get the average frequency of the sound
+    // const data = this.analyser.getAverageFrequency();
   }
 
   setupResize() {
@@ -105,8 +171,13 @@ class Sketch {
 
   render() {
     this.time += 0.02;
-
     this.material.uniforms.time.value = this.time;
+
+    // Move camera on mousemove
+    this.target.x = (1 - this.mouseX) * 0.0005;
+    this.target.y = (1 - this.mouseY) * 0.0005;
+    this.camera.rotation.x += 0.05 * (this.target.y - this.camera.rotation.x);
+    this.camera.rotation.y += 0.05 * (this.target.x - this.camera.rotation.y);
 
     this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.render.bind(this));
@@ -114,5 +185,7 @@ class Sketch {
 }
 
 new Sketch({
-  domElement: document.getElementById('webgl'),
+  canvas: document.getElementById('webgl'),
+  input: document.getElementById('input'),
+  button: document.getElementById('button'),
 });
